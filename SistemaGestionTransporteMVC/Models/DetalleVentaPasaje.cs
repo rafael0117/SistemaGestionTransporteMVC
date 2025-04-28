9b82b5b43479017728677007af8e4c1567b1e471
rafael0117 <mamanirafael324@gmail.com>
2025-04-28 17:23:04 -0500

    LayoutCliente,Creacion de Modelos y Controller

---
+++ SistemaGestionTransporteMVC/Models/DetalleVentaPasaje.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaGestionTransporteMVC.Models
+{
+	public class DetalleVentaPasaje
+	{
+		public int IdDetalle { get; set; }
+		public int Cantidad { get; set; }
+		public double Precio { get; set; }
+		public double Total { get; set; }
+		public int IdVenta { get; set; }
+		public int IdViaje { get; set; }
+	}
+}
---
+++ SistemaGestionTransporteMVC/Models/VentaPasaje.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaGestionTransporteMVC.Models
+{
+	public class VentaPasaje
+	{
+		public int IdVenta { get; set; }
+		public string Estado { get; set; } = "pendiente";
+		public DateTime FechaVenta { get; set; }
+		public double Total { get; set; }
+		public long IdUsuario { get; set; }
+		public string Numero { get; set; }
+	}
+}
